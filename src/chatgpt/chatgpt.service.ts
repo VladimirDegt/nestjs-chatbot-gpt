f8f60fddf2c15de593df3f8edf001b22fb64ef94
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-05-23 10:07:44 +0300

    feat: join gpt

--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+interface ChatGptAnswer {
+    id: string;
+    object: string;
+    created: number;
+    model: string;
+    usage: {
+        prompt_tokens: number;
+        completion_tokens: number;
+        total_tokens: number;
+    };
+    choices: {
+        message: {
+            role: string;
+            content: string;
+        };
+        finish_reason: string;
+        index: number;
+    }[];
+}
 
 @Injectable()
-export class ChatgptService {}
+export class ChatgptService {
+    private readonly logger = new Logger(ChatgptService.name);
+    private apiUrl: string;
+
+    constructor(private configService: ConfigService, private httpService: HttpService) {
+        this.apiUrl = 'https://api.openai.com/v1/chat/completions';
+    }
+
+    generateResponse(prompt: string): Observable<string> {
+        const apiKey = this.configService.get('CHATGPT_API_KEY');
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+        };
+
+        const data = {
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: prompt }],
+            temperature: 1,
+        };
+
+        return this.httpService.post(this.apiUrl, data, { headers }).pipe(
+            tap(() => this.logger.log('Request sent to OpenAI API')),
+            map(response => {
+                const choices = response.data.choices;
+                const usage = response.data.usage;
+                if (choices && choices.length > 0) {
+                    this.logger.log(`Tokens used: ${usage.total_tokens}`);
+                    return choices[0].message.content;
+                } else {
+                    throw new Error('No choices returned from OpenAI API');
+                }
+            }),
+            catchError(error => {
+                const errorMessage = error.response?.data?.error?.message || error.message || 'Unknown error';
+                this.logger.error(`Error occurred while fetching response from OpenAI API: ${errorMessage}`);
+                return of(`Error occurred while fetching response from OpenAI API: ${errorMessage}`);
+            })
+        );
+    }
+}
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,9 +1,14 @@
 import {Ctx, Message, On, Start, Update} from "nestjs-telegraf";
 import {Scenes, Telegraf} from "telegraf";
+import {ConfigService} from "@nestjs/config";
+import {ChatgptService} from "../chatgpt/chatgpt.service";
 
 type Context = Scenes.SceneContext;
 @Update()
 export class TelegramService extends Telegraf<Context> {
+    constructor( private readonly configService: ConfigService, private readonly gpt: ChatgptService) {
+        super(configService.get('TELEGRAM_BOT_TOKEN'));
+    }
     @Start()
     onStart(@Ctx() ctx: Context) {
         ctx.replyWithHTML(`<b>Hi, ${ctx.from.username}</b>
@@ -12,7 +17,7 @@ export class TelegramService extends Telegraf<Context> {
         `);
     }
     @On('text')
-    onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
-        ctx.replyWithHTML(`<i>${message}</i>`);
+    onMessage(@Message('text') message: string) {
+        return this.gpt.generateResponse(message)
     }
 }
